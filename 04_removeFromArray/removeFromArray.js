16e6b25e68c28d4ab09f330bd92fec1672d67cc1
Hrizolaminarin <hrizolaminarin@gmail.com>
2024-02-21 16:33:13 +0100

    Solution for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
+    let array = arguments[0];
 
-};
+    for (let i = 1; i < arguments.length; i++) {
+        if ( array.includes( arguments[i] ) ) {
+            array = array.filter( item => {
+                return item != arguments[i];
+            });
+        }
+    }
+
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
