e59a77ed5807bff6c12533c31dd4ba643cff545b
Hrizolaminarin <hrizolaminarin@gmail.com>
2024-02-28 10:53:10 +0100

    Solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  
+  if (isNotNumber(value)) return "ERROR";
+  
+  return Number.parseFloat( ((value - 32) * 5/9).toFixed(1) );
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+
+  if (isNotNumber(value)) return "ERROR";
+  
+  return Number.parseFloat( (value * 9/5 + 32).toFixed(1) );
 };
+
+const isNumber = function(value) {
+  return typeof value === "number";
+}
+
+const isNotNumber = function(value) {
+  return !isNumber(value);
+}
 
 // Do not edit below this line
 module.exports = {
